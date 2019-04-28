50593a86cbe6c46f7eaa8f66d168969bd4f39b6e
Alec Rios <8664387+alecrios@users.noreply.github.com>
2019-04-27 19:42:37 -0500

    Modified to return all price conversions

--- app.js
+++ app.js
@@ -6,27 +6,19 @@ const CoinGecko = require('coingecko-api');
 const app = express();
 const port = 3000;
 const coinGecko = new CoinGecko();
-
-const cacheOptions = {
+const cache = apicache.options({
 	defaultDuration: '1 minute',
 	statusCodes: {
 		include: [200],
 	},
-};
-
-const cache = apicache.options(cacheOptions).middleware;
+}).middleware;
 
 app.use(cache());
 
-app.get('/market/:coin', async (req, res) => {
-	const [error, response] = await to(coinGecko.coins.fetch(req.params.coin));
+app.get('/price', async (req, res) => {
+	const [error, response] = await to(coinGecko.coins.fetch('nano'));
 	if (!response || !response.success) throw Error(error);
-
-	res.send({
-		price: response.data.market_data.current_price.usd,
-		marketCap: response.data.market_data.market_cap.usd,
-		supply: response.data.market_data.circulating_supply,
-	});
+	res.send(response.data.market_data.current_price);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
